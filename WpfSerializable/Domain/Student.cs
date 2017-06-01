faeb76b8a18cb5a3c57af91bf05f1381cefde23b
Dimas19rus <Dimas.titan@mail.ru>
2017-06-02 00:02:14 +0700

    Add files via upload

---
+++ WpfSerializable/Domain/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    [Serializable]
+    public class Student
+    {
+        public string FullName { get; set; }
+        public string Group { get; set; }
+        public int Age { get; set; }
+    }
+}
